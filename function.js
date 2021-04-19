d179890cbb800a0087c90b5d026e3f95658c46b8
dci <rudeshdhote@gmail.com>
2021-04-19 13:28:02 +0200

    initial commit

---
+++ function.js
@@ -0,0 +1,46 @@
+function multiply(number1, number2) {
+    console.log(number1 * number2);
+
+}
+
+multiply(3,3);
+
+
+let sum =  function(number1, number2) {
+    console.log(number1 * number2);
+
+}
+
+sum(3,3);
+
+let sum1 = (number1, number2) => {
+    console.log(number1 * number2);
+
+}
+
+sum(3,3);
+
+
+
+
+function multiply1(number1, number2) {
+    console.log(number1 % number2);
+
+}
+
+multiply1(3,3);
+
+
+let sum2 =  function(number1, number2) {
+    console.log(number1 % number2);
+
+}
+
+sum2(3,3);
+
+let sum3 = (number1, number2) => {
+    console.log(number1 % number2);
+
+}
+
+sum3(3,3);
